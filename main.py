49127f10aee3822d7ba754f22832e0d4aa3ce96c
fasikakassahun <fasikakassahun1912@gmail.com>
2026-02-05 20:44:35 +0300

    Encapsulation Excample with wallet and Balance access

--- main.py
+++ main.py
@@ -1,48 +1,26 @@
-base_price = 15
-age = 21
-seat_type = 'Gold'
-show_time = 'Evening'
+class Wallet:
+   def __init__(self, balance):
+       self.__balance = balance
 
-if age > 17:
-    print('User is eligible to book a ticket')
+   def deposit(self, amount):
+       if amount > 0:
+           self.__balance += amount
 
-if age >= 21:
-    print('User is eligible for Evening shows')
-else:
-    print('User is not eligible for Evening shows')
+   def withdraw(self, amount):
+       if 0 < amount <= self.__balance:
+           self.__balance -= amount
+  
+   def get_balance(self):
+       return self.__balance
 
-is_member = False
-is_weekend = False
 
-discount = 0
-if is_member and age >= 21:
-    discount = 3
-    print('User qualifies for membership discount')
-else:
-    print('User does not qualify for membership discount')
-print('Discount:', discount)
+acct_one = Wallet(100)
+acct_one.deposit(50)
+print(acct_one.get_balance()) # 150
 
-extra_charges = 0
-if is_weekend or show_time == 'Evening':
-    extra_charges = 2
-    print('Extra charges will be applied')
-else:
-    print('No extra charges will be applied')
-print('Extra charges:', extra_charges)
+acct_two = Wallet(450)
+acct_two.withdraw(28)
+print(acct_two.get_balance()) # 422
 
-if age >= 21 or age >= 18 and (show_time != 'Evening' or is_member):
-    print('Ticket booking condition satisfied')
-
-    service_charges = 0
-    if seat_type == 'Premium':
-        service_charges = 5
-    elif seat_type == 'Gold':
-        service_charges = 3
-    else:
-        service_charges = 1
-    print('Service charges:', service_charges)
-
-    final_price = service_charges + base_price + extra_charges-discount
-    print("Final price of ticket:",final_price)
-else:
-    print('Ticket booking failed due to restrictions')
+acct_two.deposit(150)
+print(acct_two.get_balance()) # 572
